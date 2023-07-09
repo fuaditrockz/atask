a528a09de6e29d20c44249dcfb6363ca8c2fa024
Fuadit Muhammad <muhammadfuaditrockz@gmail.com>
2023-07-09 21:59:01 +0700

    Create component for repos

--- app/components/ResultCard.tsx
+++ app/components/ResultCard.tsx
@@ -1,11 +1,21 @@
 "use client";
-import Image from "next/image";
-import { GithubUsersProps } from "@/context";
 import { useContext, useEffect, useState } from "react";
-import { AtaskContext } from "@/context";
+import Image from "next/image";
 import Lottie from "react-lottie";
-import * as animationData from "../../public/avatars.json";
 import Skeleton from "react-loading-skeleton";
+import {
+  IoIosArrowDropdownCircle,
+  IoIosArrowDropupCircle,
+} from "react-icons/io";
+import { BiSolidBookBookmark } from "react-icons/bi";
+import { BsCircleFill } from "react-icons/bs";
+import { AiFillStar } from "react-icons/ai";
+import { AiOutlineBranches } from "react-icons/ai";
+
+import { GithubUsersProps } from "@/context";
+import { AtaskContext } from "@/context";
+
+import * as animationData from "../../public/avatars.json";
 import { type dataSampleProps } from "../context/dataSample";
 
 interface Card {
@@ -29,9 +39,9 @@ const ResultCard = ({
   });
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [isError, setIsError] = useState<boolean>(false);
+  const [isOpenDetails, setIsOpenDetails] = useState<boolean>(false);
 
   const getActualData = async (url: string) => {
-    setIsLoading(true);
     const response = await fetch(url);
 
     if (!response.ok) {
@@ -50,7 +60,6 @@ const ResultCard = ({
         followers: data.followers,
       });
     }
-    setIsLoading(false);
   };
 
   const getSampleData = (data: dataSampleProps) => {
@@ -63,6 +72,7 @@ const ResultCard = ({
   };
 
   useEffect(() => {
+    setIsLoading(true);
     if (userRawData) {
       if (process.env.NEXT_PUBLIC_ENV_IS_PRODUCTION) {
         getActualData(userRawData.url);
@@ -70,6 +80,9 @@ const ResultCard = ({
         getSampleData(userRawData as dataSampleProps);
       }
     }
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 1000);
   }, [userRawData]);
 
   const renderLoadingComponent = () => {
@@ -100,33 +113,81 @@ const ResultCard = ({
     );
   };
 
+  const renderRepositories = () => {
+    return [1, 2, 3, 4].map((repo, index) => {
+      return (
+        <div
+          key={index}
+          className="bg-white w-full h-auto rounded-md border border-slate-300 p-4"
+        >
+          <p className="text-[#2d3436] text-sm font-bold flex flex-row items-center gap-2">
+            <BiSolidBookBookmark /> Repository Title{" "}
+            <span className="border border-slate-200 rounded-full font-medium text-xs px-2">
+              Public
+            </span>
+          </p>
+          <p className="text-[#636e72] text-sm font-sm mb-5">
+            Some description here ya guys
+          </p>
+          <div className="flex flex-row gap-4">
+            <p className="text-[#353b48] text-sm font-medium flex flex-row gap-2 items-center">
+              <BsCircleFill color="#fdcb6e" /> Javascript
+            </p>
+            <p className="text-[#353b48] text-sm font-medium flex flex-row gap-2 items-center">
+              <AiFillStar color="#353b48" /> 38
+            </p>
+            <p className="text-[#353b48] text-sm font-medium flex flex-row gap-2 items-center">
+              <AiOutlineBranches color="#353b48" /> 11
+            </p>
+          </div>
+        </div>
+      );
+    });
+  };
+
   return isLoading ? (
     renderLoadingComponent()
   ) : isError ? null : (
-    <div className="flex items-center space-x-4">
-      <div className="flex-shrink-0">
-        <div className="h-[45px] w-[45px] flex items-center">
-          <Image
-            className="w-8 h-8 rounded-full mx-auto"
-            src={presentedData.avatar ? presentedData.avatar : "/locked.png"}
-            alt={`${presentedData.username} image`}
-            width={100}
-            height={100}
-          />
+    <>
+      <div className="flex items-center space-x-4">
+        <div className="flex-shrink-0">
+          <div className="h-[45px] w-[45px] flex items-center">
+            <Image
+              className="w-8 h-8 rounded-full mx-auto"
+              src={presentedData.avatar ? presentedData.avatar : "/locked.png"}
+              alt={`${presentedData.username} image`}
+              width={100}
+              height={100}
+            />
+          </div>
+        </div>
+        <div className="flex-1 min-w-0">
+          <p className="text-sm font-medium text-gray-900 truncate">
+            {presentedData.name ?? "No name"}
+          </p>
+          <p className="text-sm text-gray-500 truncate">
+            {presentedData.username}
+          </p>
+        </div>
+        <div className="inline-flex items-center text-base font-semibold text-gray-900">
+          <a
+            onClick={() => setIsOpenDetails(!isOpenDetails)}
+            className="cursor-pointer text-blue-600 duration-500 hover:text-blue-400"
+          >
+            {isOpenDetails ? (
+              <IoIosArrowDropupCircle size={18} />
+            ) : (
+              <IoIosArrowDropdownCircle size={18} />
+            )}
+          </a>
         </div>
       </div>
-      <div className="flex-1 min-w-0">
-        <p className="text-sm font-medium text-gray-900 truncate">
-          {presentedData.name ?? "No name"}
-        </p>
-        <p className="text-sm text-gray-500 truncate">
-          {presentedData.username}
-        </p>
-      </div>
-      <div className="inline-flex items-center text-base font-semibold text-gray-900">
-        {presentedData.followers}
-      </div>
-    </div>
+      {isOpenDetails && (
+        <div className="grid grid-cols-2 gap-4 mt-5">
+          {renderRepositories()}
+        </div>
+      )}
+    </>
   );
 };
 
